f42c68a55d90d4e85e0726e4d4718f241833fffe
CMS <awolf0412@qq.com>
2018-11-16 10:16:05 +0800

    finished config reader

--- client.py
+++ client.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+import configparser
+import cv2
+import sys
+
+if (len(sys.argv)<2) :
+    print("usage: ",sys.argv[0]," [config]")
+    exit()
+if (sys.argv[1] == '--help')|(sys.argv[1] == '-h') :
+    print("usage: ",sys.argv[0]," [config]")
+    exit()
+
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
+
